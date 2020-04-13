d303364d8045adaf87ef5d3a210a30853cb397e4
sag sag <sohrabag@kth.se>
2020-04-13 11:47:44 +0200

    added entire spring boot project

---
+++ src/main/java/com/example/VTK20200413/Vtk20200413Application.java
@@ -0,0 +1,13 @@
+package com.example.VTK20200413;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vtk20200413Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vtk20200413Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VTK20200413/Vtk20200413ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VTK20200413;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vtk20200413ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
